7e2d5e11b637e9ad83bf9f825cbb94e0e67dc4a2
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-02-09 17:00:28 +0000

    manifest (feat): add upload.sh

---
+++ upload.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export BOOK_CLIENT_SECRET=$(cat ~/secret/v0/book.pat)
+export BOOK_CLIENT_TOKEN_TTL=5m
+export BOOK_CLIENT_TOKEN_ADMIN=true
+export BOOK_CLIENT_TOKEN_AUD=https://dev.practable.io/book
+export BOOK_CLIENT_TOKEN_SUB=admin
+
+export BOOK_CLIENT_TOKEN=$(book token)
+export BOOK_CLIENT_SCHEME=https
+export BOOK_CLIENT_HOST=dev.practable.io
+export BOOK_CLIENT_BASE_PATH=/book/api/v1
+export BOOK_CLIENT_FORMAT=YAML
+
+book manifest check manifest.yaml
+if [ $? -eq 0 ]; then
+   book manifest replace manifest.yaml
+else
+    echo "Manifest check returned errors, did not upload"
+fi
+
